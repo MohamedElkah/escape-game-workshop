42140a1560fed9b37af89cfca7a3ab4f11b85d20
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-10-10 14:18:23 +0200

    put rules directly in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div
-    class="min-h-screen flex flex-col items-center justify-center bg-gray-100"
+    class="min-h-screen flex flex-col items-center justify-center bg-gray-100 gap-8"
   >
-    <p class="text-4xl mb-8 bg-red-300">
+    <p class="text-4xl bg-red-300">
 		  Bienvenue dans l'Escape Room !
     </p>
-		<p>
+		<p class="w-[700px] mx-auto text-justify">
 			L'expérience que vous vous apprêtez à expérimenter est à prendre au sérieux.
 			Ici, vous apprendrez à identifier et gérer le harcèlement au travail.
 			Nous vous présenterons plusieurs situations qu'on peut retrouver dans le monde professionnel
