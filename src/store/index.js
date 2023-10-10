e21617ce6ae792ad7ea6f1a5883c36f95cebb4d4
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-10-10 17:25:57 +0200

    update store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
 import { defineStore } from 'pinia'
+import data from "@/data/index.json";
 
-const useStore = defineStore('main',{
+const useRoomStore = defineStore('room',{
     state: () => ({
-        text: "Hello World!"
+        currentRoomId: 0,
     }),
+    getters: {
+        currentRoom(state) {
+            return data.rooms[state.currentRoomId];
+        },
+    },
     actions: {
+        setCurrentRoomId(id) {
+            if (id < data.rooms.length && id >= 0) {
+                this.currentRoomId = id;
+            }
+        }
     },
 })
 
-export default useStore;
+export default useRoomStore;
