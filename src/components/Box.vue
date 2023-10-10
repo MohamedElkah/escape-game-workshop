c2934f0a1a527127310a3fd93e541ed256c48f5b
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-10-10 17:16:48 +0200

    custom component for box ui

---
+++ src/components/Box.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+	<div class="dialogue p-4 bg-white rounded-md flex flex-col w-5/6 gap-5 max-w-[900px] mx-auto relative">
+		<slot></slot>
+	</div>
+</template>
