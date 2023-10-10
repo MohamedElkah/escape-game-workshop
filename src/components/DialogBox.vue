1eb8834bae1c029cf9a9cdf3da796158661d50fb
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-10-10 17:23:52 +0200

    replace by box component & update question box

--- src/components/DialogBox.vue
+++ src/components/DialogBox.vue
@@ -1,78 +1,82 @@
 <template>
-  <div class="room flex justify-center items-center min-h-screen bg-blue-200">
-    <div v-if="currentDialogue" class="dialogue p-4 bg-white rounded-md flex flex-col w-[900px] mx-auto relative">
-      <h2 class="text-lg font-bold w-fit bg-black text-white rounded-full py-1 px-3 absolute left-0 -translate-y-1/2 top-0">{{ currentDialogue.speaker }}</h2>
-      <p class="text-md mt-2.5">{{ displayedText }}</p>
-      <ButtonComponent :action="advanceDialogue" classes="self-end mt-5">
-        Suivant
-      </ButtonComponent>
-    </div>
-  </div>
+	<div class="room flex justify-center items-center min-h-screen bg-blue-200">
+		<Box v-if="currentDialogue">
+			<h2 class="text-lg font-bold w-fit bg-black text-white rounded-full py-1 px-3 absolute left-0 -translate-y-1/2 top-0">{{ currentDialogue.speaker }}</h2>
+			<p class="text-md mt-2.5">{{ displayedText }}</p>
+			<ButtonComponent :action="advanceDialogue" classes="self-end mt-5">
+				Suivant
+			</ButtonComponent>
+		</Box>
+	</div>
 </template>
 
 <script>
 import {ref, computed, onMounted, onUnmounted} from "vue";
 import ButtonComponent from "./Button.vue";
+import Box from "./Box.vue";
 
 export default {
-  components: {
-    ButtonComponent,
-  },
-  props: {
-    dialogues: Array
-  },
-  setup(props, _) {
+	components: {
+		ButtonComponent,
+		Box
+	},
+	props: {
+		dialogues: Array,
+		noMoreDialogAction: Function
+	},
+	setup(props, _) {
 
-    const currentIndex = ref(0);
-    const displayLength = ref(1);
-    const maxDisplayLength = 1; // A augmenter pour un effet de machine à écrire plus rapide
+		const currentIndex = ref(0);
+		const displayLength = ref(1);
+		const maxDisplayLength = 1; // A augmenter pour un effet de machine à écrire plus rapide
 
-    let interval;
+		let interval;
 
-    const advanceDialogue = () => {
-      clearInterval(interval);
-      // Si le message n'est pas encore entièrement affiché, on l'affiche en entier
-      if (
-        displayLength.value < props.dialogues[currentIndex.value].message.length
-      ) {
-        interval = setInterval(() => {
-          displayLength.value = Math.min(
-            displayLength.value + maxDisplayLength,
-			  props.dialogues[currentIndex.value].message.length
-          );
+		const advanceDialogue = () => {
+			clearInterval(interval);
+			// Si le message n'est pas encore entièrement affiché, on l'affiche en entier
+			if (
+				displayLength.value < props.dialogues[currentIndex.value].message.length
+			) {
+				interval = setInterval(() => {
+					displayLength.value = Math.min(
+						displayLength.value + maxDisplayLength,
+						props.dialogues[currentIndex.value].message.length
+					);
 
-          if (
-            displayLength.value >=
-			  props.dialogues[currentIndex.value].message.length
-          ) {
-            clearInterval(interval);
-          }
-        }, 50);
-        // Sinon, si on est au dernier message, on ne fait rien
-      } else if (currentIndex.value < props.dialogues.length - 1) {
-        currentIndex.value++;
-        displayLength.value = 1;
-        // Appel récursif pour passer au message suivant
-        advanceDialogue();
-      }
-    };
+					if (
+						displayLength.value >=
+						props.dialogues[currentIndex.value].message.length
+					) {
+						clearInterval(interval);
+					}
+				}, 50);
+				// Sinon, si on est au dernier message, on ne fait rien
+			} else if (currentIndex.value < props.dialogues.length - 1) {
+				currentIndex.value++;
+				displayLength.value = 1;
+				props.noMoreDialogAction();
+				// Appel récursif pour passer au message suivant
+				advanceDialogue();
+			}
+		};
 
-    onMounted(() => {
-      advanceDialogue();
-    });
+		onMounted(() => {
+			advanceDialogue();
+		});
 
-    onUnmounted(() => {
-      clearInterval(interval);
-    });
+		onUnmounted(() => {
+			clearInterval(interval);
+		});
 
-    const currentDialogue = computed(() => props.dialogues[currentIndex.value]);
+		const currentDialogue = computed(() => props.dialogues[currentIndex.value]);
 
-    const displayedText = computed(() =>
-      currentDialogue.value.message.substring(0, displayLength.value)
-    );
+		const displayedText = computed(() =>
+			currentDialogue.value.message.substring(0, displayLength.value)
+		);
 
-    return {currentDialogue, displayedText, advanceDialogue};
-  },
+		return {currentDialogue, displayedText, advanceDialogue};
+	},
 };
 </script>
 
