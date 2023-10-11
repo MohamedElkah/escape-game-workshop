0ecf7e74c52a5f30a9812caae5134fd4622969de
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-10-11 10:25:50 +0200

    update

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,64 +1,63 @@
 <template>
-  <div class="room flex justify-center items-center min-h-screen bg-blue-200">
-    <Box>
-      <h2
-        class="text-lg font-bold w-fit bg-black text-white rounded-full py-1 px-3 mx-auto"
-      >
-        {{ theme }}
-      </h2>
-      <p class="text-center">{{ situation.scenario }}</p>
-      <ul class="grid grid-cols-2 gap-3">
-        <li
-          v-for="(response, index) in situation.responses.sort(() =>
-            Math.random() > 0.5 ? 1 : -1
-          )"
-          :key="index"
-          class="h-full"
-        >
-          <ButtonComponent
-            :classes="buttonClass(index) + ' w-full h-full'"
-            :action="() => handleClick(response.is_correct, index)"
-          >
-            {{ response.text }}
-          </ButtonComponent>
-        </li>
-      </ul>
-    </Box>
-  </div>
+	<div class="room flex justify-center items-center min-h-screen bg-blue-200">
+		<Box>
+			<h2 class="text-lg font-bold w-fit bg-black text-white rounded-full py-1 px-3 mx-auto">{{ theme }}</h2>
+			<p class="text-center">{{ situation.scenario }}</p>
+			<ul class="grid grid-cols-2 gap-3">
+				<li
+					v-for="(response, index) in situation.responses.sort(() => (Math.random() > .5) ? 1 : -1)"
+					:key="index"
+					class="h-full"
+				>
+					<ButtonComponent
+						:classes="buttonClass(index) + ' w-full h-full'"
+						:action="() => $emit('answer', response.id)"
+					>
+						{{ response.text }}
+					</ButtonComponent>
+				</li>
+			</ul>
+		</Box>
+	</div>
 </template>
 
 <script>
-import {ref, defineEmits} from "vue";
+import {ref} from "vue";
 import Box from "./Box.vue";
 import ButtonComponent from "./Button.vue";
+import useRoomStore from "@/store/roomStore.js";
 
 export default {
-  components: {Box, ButtonComponent},
-  props: {
-    situation: Object,
-    theme: String,
-  },
-  setup() {
-    const emit = defineEmits();
-    const selectedAnswerIndex = ref(null); // pour stocker l'index de la réponse sélectionnée
-    const wasAnswerCorrect = ref(false); // pour stocker si la réponse sélectionnée est correcte ou non
+	components: {Box, ButtonComponent},
+	props: {
+		situation: Object,
+		theme: String,
+	},
+	setup(props) {
+		const selectedAnswerIndex = ref(null); // pour stocker l'index de la réponse sélectionnée
+		const wasAnswerCorrect = ref(false); // pour stocker si la réponse sélectionnée est correcte ou non
+		const {currentRoomId, addAnswer} = useRoomStore()
 
-    const handleClick = (isCorrect, index) => {
-      selectedAnswerIndex.value = index;
-      wasAnswerCorrect.value = isCorrect;
-      emit("answer", isCorrect);
-    };
+		const handleClick = (isCorrect, index) => {
+			selectedAnswerIndex.value = index;
+			wasAnswerCorrect.value = isCorrect;
+			addAnswer({
+				room: currentRoomId.value,
+				situation: props.situation.id,
+				answer: index,
+			})
+		};
 
-    const buttonClass = (index) => {
-      if (index !== selectedAnswerIndex.value) return "";
-      return wasAnswerCorrect.value ? "bg-green-500" : "bg-red-500";
-    };
+		const buttonClass = (index) => {
+			if (index !== selectedAnswerIndex.value) return "";
+			return wasAnswerCorrect.value ? "bg-green-500" : "bg-red-500";
+		};
 
-    return {
-      handleClick,
-      buttonClass,
-    };
-  },
+		return {
+			handleClick,
+			buttonClass,
+		};
+	},
 };
 </script>
 
--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,123 +1,117 @@
 <template>
-  <div class="room" v-if="room">
-    <div
-      class="background"
-      :style="{
-        backgroundImage: `url(${
-          currentRoom.situations?.[0]
-            ? imgFolder + currentRoom.situations?.[0].background
-            : ''
-        })`,
-      }"
-    >
-      <img
-        :src="
-          currentRoom.situations?.[0]
-            ? imgFolder + currentRoom.situations[0].sprite
-            : ''
-        "
-        alt="sprite"
-        class="sprite"
-      />
-    </div>
-
-    <DialogBox
-      :message="currentMessage"
-      @next="handleNext"
-      v-if="showDialog"
-      :dialogues="currentRoom.dialog"
-      :no-more-dialog-action="noMoreDialogAction"
-    />
-    <QuestionBox
-      :theme="currentRoom.theme"
-      :situation="currentSituation"
-      @answer="handleAnswer"
-      v-else-if="!showDialog"
-    />
-  </div>
+	<div class="room" v-if="currentRoom">
+		<div class="background"
+			 :style="{
+				backgroundImage: `url(${currentSituation ? imgFolder + currentSituation.background : ''})`
+			 }"
+		>
+			<img
+				:src="currentSituation ? imgFolder + currentSituation.sprite : ''"
+				alt="sprite"
+				class="sprite"
+			/>
+		</div>
+		<DialogBox
+			:message="currentMessage"
+			@next="handleNext"
+			v-if="showDialog"
+			:dialogues="currentRoom.dialog"
+			:no-more-dialog-action="noMoreDialogAction" />
+		<QuestionBox
+			:theme="currentRoom.theme"
+			:situation="currentSituation"
+			@answer="handleAnswer"
+			v-else-if="!showDialog"
+		/>
+	</div>
 </template>
 
 <script>
 import {ref, onMounted, computed} from "vue";
-import jsonData from "@/data/index.json";
 import DialogBox from "@/components/DialogBox.vue";
 import QuestionBox from "@/components/QuestionBox.vue";
 import useRoomStore from "@/store/roomStore.js";
-import useQuestionStore from "@/store/questionStore.js";
+import jsonData from "@/data/index.json";
 
 export default {
-  components: {
-    DialogBox,
-    QuestionBox,
-  },
-  setup() {
-    const {currentRoomId, currentRoom} = useRoomStore();
-    const showDialog = ref(true);
-    const currentMessage = ref("Chargement...");
-    const imgFolder = ref("");
-    const room = computed(() => {
-      return jsonData.rooms[currentRoomId];
-    });
-    const currentSituation = computed(() => {
-      return currentRoom.situations[0];
-    });
-    const {question, responses, generateQuestion, loading, error} =
-      useQuestionStore();
+	components: {
+		DialogBox,
+		QuestionBox,
+	},
+	setup() {
+		const {currentRoomId, currentRoom, addAnswer} = useRoomStore()
+		const showDialog = ref(true);
+		const currentMessage = ref("Chargement...");
+		const imgFolder = ref("")
+		const currentSituationId = ref(0);
+		const currentSituation = computed(() => {
+			return currentRoom.situations[currentSituationId.value];
+		});
 
-    onMounted(() => {
-      try {
-        imgFolder.value = jsonData.imagesFolder;
-        currentMessage.value = "Bienvenue dans cette room !";
-      } catch (err) {
-        console.error("Erreur lors de la manipulation des données:", err);
-        currentMessage.value = "Erreur lors de la manipulation des données.";
-      }
-    });
+		onMounted(() => {
+			try {
+				imgFolder.value = jsonData.imagesFolder;
+				currentMessage.value = "Bienvenue dans cette room !";
+			} catch (err) {
+				console.error("Erreur lors de la manipulation des données:", err);
+				currentMessage.value = "Erreur lors de la manipulation des données.";
+			}
+		});
 
-    const handleNext = () => {
-      showDialog.value = false;
-    };
+		const handleNext = () => {
+			showDialog.value = false;
+		};
+		const handleAnswer = (index) => {
+			console.log(currentRoomId)
+			addAnswer({
+				roomId: currentRoomId,
+				situationId: currentSituation.value.id,
+				answerId: index,
+			})
+			// if (isCorrect) {
+			// 	currentMessage.value = "Correct ! Passons à la situation suivante.";
+			// } else {
+			// 	currentMessage.value = "Incorrect. Réessayez.";
+			// }
+			// showDialog.value = true;
+			if (currentSituationId.value < currentRoom.situations.length - 1) {
+				currentSituationId.value++;
+			} else {
+				console.log("test goToNextSituation() else")
+			}
+			// }
+		};
 
-    const handleAnswer = (isCorrect) => {
-      if (isCorrect) {
-        currentMessage.value = "Correct ! Passons à la situation suivante.";
-      } else {
-        currentMessage.value = "Incorrect. Réessayez.";
-      }
-      showDialog.value = true;
-    };
+		const noMoreDialogAction = () => {
+			showDialog.value = false;
+		};
 
-    const noMoreDialogAction = () => {
-      showDialog.value = false;
-    };
-
-    return {
-      room,
-      imgFolder,
-      currentRoom,
-      currentMessage,
-      currentSituation,
-      showDialog,
-      handleNext,
-      handleAnswer,
-      noMoreDialogAction,
-    };
-  },
+		return {
+			imgFolder,
+			currentRoom,
+			currentMessage,
+			currentSituation,
+			showDialog,
+			handleNext,
+			handleAnswer,
+			noMoreDialogAction,
+		};
+	},
 };
 </script>
 
 <style scoped>
 .background {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-size: cover;
-  background-position: center;
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background-size: cover;
+	background-position: center;
 }
 
 .sprite {
-  position: absolute;
-  width: 150px;
-  height: 150px;
+	position: absolute;
+	width: 150px;
+	height: 150px;
 }
 </style>
