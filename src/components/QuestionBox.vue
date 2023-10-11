86a0efceae56debb82e7c0f69d8581542863b4c4
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-10-11 11:21:00 +0200

    add "no more situations" state

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,22 +1,41 @@
 <template>
 	<div class="room flex justify-center items-center min-h-screen bg-blue-200">
 		<Box>
-			<h2 class="text-lg font-bold w-fit bg-black text-white rounded-full py-1 px-3 mx-auto">{{ theme }}</h2>
-			<p class="text-center">{{ situation.scenario }}</p>
-			<ul class="grid grid-cols-2 gap-3">
-				<li
-					v-for="(response, index) in situation.responses.sort(() => (Math.random() > .5) ? 1 : -1)"
-					:key="index"
-					class="h-full"
-				>
+			<template v-if="hasNoMoreSituation">
+				<p>Plus de situations</p>
+				<div class="flex items-center gap-3">
 					<ButtonComponent
-						:classes="buttonClass(index) + ' w-full h-full'"
-						:action="() => $emit('answer', response.id)"
+						classes="flex-1 h-full"
+						:action="onNextRoom"
 					>
-						{{ response.text }}
+						Room suivante
 					</ButtonComponent>
-				</li>
-			</ul>
+					<ButtonComponent
+						classes="flex-1 h-full"
+						:is-secondary="true"
+					>
+						Voir les résultats
+					</ButtonComponent>
+				</div>
+			</template>
+			<template v-else>
+				<h2 class="text-lg font-bold w-fit bg-black text-white rounded-full py-1 px-3 mx-auto">{{ theme }}</h2>
+				<p class="text-center">{{ situation.scenario }} {{hasNoMoreSituation ? "No More" : "More"}}</p>
+				<ul class="grid grid-cols-2 gap-3">
+					<li
+						v-for="(response, index) in situation.responses.sort(() => (Math.random() > .5) ? 1 : -1)"
+						:key="index"
+						class="h-full"
+					>
+						<ButtonComponent
+							:classes="buttonClass(index) + ' w-full h-full'"
+							:action="() => $emit('answer', response.id)"
+						>
+							{{ response.text }}
+						</ButtonComponent>
+					</li>
+				</ul>
+			</template>
 		</Box>
 	</div>
 </template>
@@ -32,6 +51,8 @@ export default {
 	props: {
 		situation: Object,
 		theme: String,
+		hasNoMoreSituation: Boolean,
+		onNextRoom: Function
 	},
 	setup(props) {
 		const selectedAnswerIndex = ref(null); // pour stocker l'index de la réponse sélectionnée
