e776d081dbf3bd0334baa75c8345f9cf95510076
Wilfried <122891191+Jukiyoomi@users.noreply.github.com>
2023-10-12 10:35:04 +0200

    update homeview

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,19 @@
     class="min-h-screen flex flex-col items-center justify-center bg-gray-100 gap-8"
   >
     <p class="text-4xl bg-red-300">Bienvenue dans l'Escape Room !</p>
-    <p class="w-[700px] mx-auto text-justify">
-      L'expérience que vous vous apprêtez à expérimenter est à prendre au
-      sérieux. Ici, vous apprendrez à identifier et gérer le harcèlement au
-      travail. Nous vous présenterons plusieurs situations qu'on peut retrouver
-      dans le monde professionnel et vous devrez y répondre en choisissant la
-      meilleure réponse selon vous.
-    </p>
-    <ButtonComponent :action="goToRoom"> Commencer </ButtonComponent>
+    <div class="flex flex-row-reverse gap-3">
+      <div class="flex flex-col justify-between">
+        <p class="w-[400px] mx-auto text-justify">
+          L'expérience que vous vous apprêtez à expérimenter est à prendre au
+          sérieux. Ici, vous apprendrez à identifier et gérer le harcèlement au
+          travail. Nous vous présenterons plusieurs situations qu'on peut retrouver
+          dans le monde professionnel et vous devrez y répondre en choisissant la
+          meilleure réponse selon vous.
+        </p>
+        <ButtonComponent :action="goToRoom"> Commencer </ButtonComponent>
+      </div>
+      <img src="@/assets/sprites/wink.png" class="h-[300px]" alt="home sprite">
+    </div>
   </div>
 </template>
 
